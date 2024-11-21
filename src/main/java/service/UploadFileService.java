498f934ddae9065d76fdc0264f4250bc9e468fd2
hoanganhw2 <hoanganh9z570@gmail.com>
2024-11-21 19:22:16 +0700

    111

--- src/main/java/service/UploadFileService.java
+++ src/main/java/service/UploadFileService.java
@@ -3,6 +3,7 @@ package service;
 public class UploadFileService {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("now");
+		System.out.println("now13343");
+		System.out.println("Hoang anh ");
 	}
 }
